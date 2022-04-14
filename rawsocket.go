5ea445146a8dfce70211a7a1da1a3840d74df082
Dennis Ping <idennisping@gmail.com>
2022-04-14 13:37:15 -0400

    Implement raw sockets with good error checking

--- rawsocket.go
+++ rawsocket.go
@@ -1,23 +1,48 @@
 package main
 
-import "syscall"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"syscall"
+)
 
-func InitSocketSender(addr *syscall.SockaddrInet4) int {
-	sock_send, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
-	if err != nil {
-		panic(err)
+func InitSendSocket(address string, port int) (int, error) {
+	if strings.Count(address, ".") != 4 {
+		return -1, fmt.Errorf("invalid IPv4 address: %s", address)
 	}
-	err = syscall.Connect(sock_send, addr)
-	if err != nil {
-		panic(err)
+	ip := strings.Split(address, ".") // Convert address into [4]byte
+	var ip_bytes [4]byte
+	for i := 0; i < 4; i++ {
+		b, err := strconv.Atoi(ip[i])
+		if err != nil {
+			return -1, err
+		}
+		ip_bytes[i] = byte(b)
 	}
-	return sock_send
+	addr := new(syscall.SockaddrInet4)
+	addr.Port = port
+	addr.Addr = ip_bytes
+
+	send_socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+	if err != nil {
+		return -1, fmt.Errorf("error creating sender socket: %s", err.Error())
+	}
+	err = syscall.SetsockoptInt(send_socket, syscall.IPPROTO_IP, syscall.IP_HDRINCL, 1)
+	if err != nil {
+		return -1, fmt.Errorf("error setting IP_HDRINCL: %s", err.Error())
+	}
+	err = syscall.Connect(send_socket, addr)
+	if err != nil {
+		return -1, fmt.Errorf("error connecting to %s:%d: %s", address, port, err.Error())
+	}
+	return send_socket, nil
 }
 
-func InitSocketReceiver() int {
-	sock_recv, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
+func InitRecvSocket() (int, error) {
+	recv_socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
 	if err != nil {
-		panic(err)
+		return -1, fmt.Errorf("error creating receiver socket: %s", err.Error())
 	}
-	return sock_recv
+	return recv_socket, nil
 }
