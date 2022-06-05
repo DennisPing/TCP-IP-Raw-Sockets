c579c4ab8e363e61fbb19a51636acfbe6ec6efb5
Dennis Ping <idennisping@gmail.com>
2022-06-05 16:31:53 -0400

    Add better diff print statements

--- diff.py
+++ diff.py
@@ -10,14 +10,26 @@ def main():
     parser.add_argument("file2", type=str, help="Second file to compare")
     args = parser.parse_args()
 
-    file1 = args.file1
-    file2 = args.file2
+    file1: str = args.file1
+    file2: str = args.file2
 
-    subprocess.run(['diff', file1, file2])
+    if not os.path.isfile(file1):
+        print(f"{file1} does not exist")
+        return 1
+    if not os.path.isfile(file2):
+        print(f"{file2} does not exist")
+        return 1
+
+    output = subprocess.run(['diff', file1, file2], capture_output=True)
+    decoded = output.stdout.decode('utf-8')
+    if decoded:
+        print(decoded)
+    else:
+        print("The two files are identical")
 
     size1 = os.path.getsize(file1)
     size2 = os.path.getsize(file2)
-    print(f"{file1} = {size1} bytes\n{file2} = {size2} bytes")
+    print(f"{file1}:\t{size1} bytes\n{file2}:\t{size2} bytes")
 
 if __name__ == "__main__":
     main()
