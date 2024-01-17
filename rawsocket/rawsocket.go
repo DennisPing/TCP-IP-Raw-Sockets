d3dedaccd7f3cd6b2e14093d02772519e88fe89b
Dennis Ping <idennisping@gmail.com>
2024-01-17 17:27:18 -0500

    Implemented receive buffer size and set timeout.

--- rawsocket/rawsocket.go
+++ rawsocket/rawsocket.go
@@ -3,6 +3,7 @@ package rawsocket
 import (
 	"fmt"
 	"syscall"
+	"time"
 )
 
 // InitSendSocket creates a raw socket for sending packets.
@@ -26,5 +27,35 @@ func InitRecvSocket() (int, error) {
 	if err != nil {
 		return -1, fmt.Errorf("unable to create recv socket: %w", err)
 	}
+
+	// Linux commands
+	// sysctl net.core.rmem_default => default SO_RCVBUF
+	// sysctl net.core.rmem_max => max SO_RCVBUF
+
+	// Set the receive buffer size
+	err = syscall.SetsockoptInt(sockFd, syscall.SOL_SOCKET, syscall.SO_RCVBUF, 1024*1024) // 1 MB
+	if err != nil {
+		syscall.Close(sockFd)
+		return -1, fmt.Errorf("unable to set recv buffer size: %w", err)
+	}
 	return sockFd, nil
+}
+
+// SetTimeout sets a receive timeout on the socket file descriptor.
+func SetTimeout(fd int, duration time.Duration) error {
+	sec := int64(duration.Seconds())
+	usec := (duration % time.Second).Microseconds()
+
+	// Create a Timeval struct
+	tv := syscall.Timeval{
+		Sec:  sec,
+		Usec: usec,
+	}
+
+	err := syscall.SetsockoptTimeval(fd, syscall.SOL_SOCKET, syscall.SO_RCVTIMEO, &tv)
+	if err != nil {
+		return fmt.Errorf("setsockopt SO_RCVTIMEO: %w", err)
+	}
+
+	return nil
 }
