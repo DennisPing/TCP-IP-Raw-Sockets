82314a85bddbf66377d37b6a38095c7a9d5ef1e5
Dennis Ping <idennisping@gmail.com>
2024-01-17 17:26:53 -0500

    Updated Main to use the http Client.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/url"
 	"os"
 	"path"
 
@@ -19,7 +18,7 @@ func cliUsage() {
 
 func main() {
 	var (
-		input_url string
+		inputUrl string
 	)
 
 	flag.Usage = cliUsage
@@ -28,7 +27,7 @@ func main() {
 
 	// Validate input arguments
 	if flag.NArg() == 1 {
-		input_url = flag.Arg(0)
+		inputUrl = flag.Arg(0)
 	} else if flag.NArg() < 1 {
 		fmt.Println("Error: missing URL")
 		cliUsage()
@@ -38,37 +37,33 @@ func main() {
 		cliUsage()
 		os.Exit(1)
 	}
-	u, err := url.Parse(input_url)
+
+	client := myhttp.NewClient()
+	resp, err := client.Get(inputUrl)
 	if err != nil {
-		fmt.Printf("Invalid URL: %s\n", err)
+		fmt.Printf("GET request error: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Make the GET request
-	res, err := myhttp.Get(u)
-	if err != nil {
-		fmt.Printf("GET request error: %s\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%d %s\n", res.StatusCode, res.Reason)
-	if res.StatusCode != 200 {
+	fmt.Printf("%d %s\n", resp.StatusCode, resp.Reason)
+	if resp.StatusCode != 200 {
 		fmt.Printf("Did not get \"200 OK\" response. Exiting...\n")
 		os.Exit(1)
 	}
 
 	// Write response body to file
-	var fname string = path.Base(u.Path)
-	f, err := os.Create(fname)
+	fileName := path.Base(resp.Url.Path)
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer f.Close()
-	_, err = f.Write(res.Body)
+
+	_, err = f.Write(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("Wrote %d bytes to %s\n", len(res.Body), fname)
+	fmt.Printf("Wrote %d bytes to %s\n", len(resp.Body), fileName)
 }
