7125f0286ecfcaf0e225f4910d25130e415a1dc9
Dennis Ping <idennisping@gmail.com>
2024-01-17 19:56:22 -0500

    Removed unused unit tests.

--- http/doubly_linked_list_test.go
+++ http/doubly_linked_list_test.go
@@ -8,69 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInsertNode_AddToEmpty(t *testing.T) {
-	dll := NewDoublyLinkedList()
-
-	// Create a new node with a sequence number and payload.
-	newNode := &Node{
-		seqNum:  1,
-		payload: []byte("node1"),
-	}
-
-	// Insert the node into the empty list.
-	dll.InsertNode(newNode)
-
-	assert.Equal(t, newNode, dll.head)
-	assert.Equal(t, newNode, dll.tail)
-	assert.Nil(t, newNode.prev)
-	assert.Nil(t, newNode.next)
-	assert.Equal(t, len(newNode.payload), dll.totalLen)
-
-	expectedBytes := []byte("node1")
-	actualBytes := dll.ToBytes()
-	assert.Equal(t, expectedBytes, actualBytes)
-}
-
-func TestInsertNode_OrderedInsertion(t *testing.T) {
-	dll := NewDoublyLinkedList()
-
-	nodes := []*Node{
-		{seqNum: 1, payload: []byte("node1")},
-		{seqNum: 2, payload: []byte("node2")},
-		{seqNum: 3, payload: []byte("node3")},
-	}
-
-	for _, node := range nodes {
-		dll.InsertNode(node)
-	}
-
-	current := dll.head
-	expectedTotalLen := 0
-	for i, expectedNode := range nodes {
-		assert.Equal(t, expectedNode, current)
-		if i > 0 { // For nodes after the first one, check the prev pointer.
-			assert.Equal(t, nodes[i-1], current.prev)
-		} else {
-			assert.Nil(t, current.prev) // The first node's prev should be nil.
-		}
-		expectedTotalLen += len(expectedNode.payload)
-		current = current.next
-	}
-	assert.Nil(t, current)                         // After the last node, current should be nil.
-	assert.Equal(t, nodes[len(nodes)-1], dll.tail) // The last node should be the tail.
-
-	// Verify the total length of the payloads in the list.
-	assert.Equal(t, expectedTotalLen, dll.totalLen)
-
-	// Verify the combined payload using ToBytes.
-	var expectedBytes []byte
-	for _, node := range nodes {
-		expectedBytes = append(expectedBytes, node.payload...)
-	}
-	actualBytes := dll.ToBytes()
-	assert.Equal(t, expectedBytes, actualBytes)
-}
-
 func TestInsertNode(t *testing.T) {
 	tests := []struct {
 		name          string
